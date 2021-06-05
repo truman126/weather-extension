a75d6f3acd774821cf239378847ae8f126295736
truman126 <trumanjohnston@gmail.com>
2021-06-05 00:18:01 -0400

    everything working, working on aesthetics and night mode implementation

--- weather.js
+++ weather.js
@@ -3,7 +3,6 @@ var region = "land";
 var temp = 666;
 var desc = "cloudy with a chance of meatballs";
 var icon = 0;
-import { getIcon } from './icons.js';
 
 function check() {
   var data = localStorage.getItem("weatherReport");
@@ -23,25 +22,25 @@ function check() {
     //the time has exceeded, a weather update is needed
     updateReport();
   }
-
-  updateHTML();
 }
 
 function updateReport() {
   console.log("updating the report");
   //fetch the weather from wttr.in
   fetch("https://wttr.in/?format=j1")
-    .then((response) => response.txt())
+    .then(response => response.json())
     .then((data) => {
       //create the JSON of the weather report
-      var report = {
-        time: new Date(),
-        temp_C: data.current_condition[0].temp_C,
-        temp_F: data.current_condition[0].temp_F,
-        city: data.nearest_area[0].areaName[0].value,
-        region: data.nearest_area[0].region[0].value,
-        desc: data.current_condition[0].weatherDesc[0].value,
-        icon: 3,
+      let desc=  data.current_condition[0].weatherDesc[0].value;
+      let icon = getIcon(desc);
+
+      let report = {
+        "time": new Date(),
+        "temp_C": data.current_condition[0].temp_C,
+        "temp_F": data.current_condition[0].temp_F,
+        "city": data.nearest_area[0].areaName[0].value,
+        "region": data.nearest_area[0].region[0].value,
+        "icon": icon,
       };
 
       //store report in an instance
@@ -51,16 +50,14 @@ function updateReport() {
 
 function updateHTML() {
   let data = JSON.parse(localStorage.getItem("weatherReport"));
+  console.log(data);
 
   document.getElementById("weatherIcon").innerHTML = data.icon;
   document.getElementById("weatherTemp").innerHTML = data.temp_C + " &#x00B0;C";
-  document.getElementById("location").innerHTML = data.city + ", " + region;
-}
-
-function matchDesc(desc) {
-  var fr = new FileReader();
-  fr.readAsText('./icons.txt');
-  console.log('hello');
+  document.getElementById("location").innerHTML = "&#x1F4CC; " + data.city + ", " + data.region;
 
 }
-console.log(getIcon);
+document.addEventListener("DOMContentLoaded", function() {
+  check();
+  updateHTML();
+});
