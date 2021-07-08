5eea9c644be1d5416887ae07f4bd23bb855873c3
truman126 <trumanjohnston@gmail.com>
2021-07-08 01:15:53 -0400

    progress update

--- icons.js
+++ icons.js
@@ -1,58 +1,88 @@
+function scan(icons, iconArr){
+    let result = "";
+    let tempResult = "";
+
+    for (i = 0; i < iconArr.length; i++){
+        tempResult = icons[iconArr[i].toLowerCase()];
+
+        if (tempResult !== undefined){
+            result += tempResult;
+            
+
+        }
+    }
+    
+    return result;
+    
+
+}
+
 function getIcon(desc){
-    let icons = {
-        "Clear" : "&#x1F324;",
-        "Sunny" : "&#x2600;",
-        "Partly cloudy" : "&#x26C5;",
-        "Cloudy" : "&#x2601;",
-        "Overcast" : "&#x2601;",
-        "Mist" : "&#x1F4A7;",
-        "Patchy rain possible" : "&#x1F327;",
-        "Patchy snow possible" : "&#x1F328;",
-        "Patchy sleet possible" : "&#x1F328;",
-        "Patchy freezing drizzle possible" : "&#x1F327;",
-        "Thundery outbreaks possible" : "&#x1F329;",
-        "Blowing snow" : "&#x2744;",
-        "Blizzard" : "&#x2744;",
-        "Fog" : "&#x1F32B;",
-        "Freezing fog" : "&#x1F32B;",
-        "Patchy light drizzle" : "&#x1F327;",
-        "Light drizzle" : "&#x1F327;",
-        "Freezing drizzle" : "&#x1F327;",
-        "Heavy freezing drizzle" : "&#x1F327;",
-        "Patchy light rain" : "&#x1F327;",
-        "Light rain" : "&#x1F327;",
-        "Moderate rain at times" : "&#x1F327;",
-        "Moderate rain" : "&#x1F327;",
-        "Heavy rain at times" : "&#x1F327;",
-        "Heavy rain" : "&#x1F327;",
-        "Light freezing rain" : "&#x1F327;",
-        "Moderate or heavy freezing rain" : "&#x1F327;",
-        "Light sleet" : "&#x1F328;",
-        "Moderate or heavy sleet" : "&#x1F328;",
-        "Patchy light snow" : "&#x1F328;",
-        "Light snow" : "&#x1F328;",
-        "Patchy moderate snow" : "&#x1F328;",
-        "Moderate snow" : "&#x1F328;",
-        "Patchy heavy snow" : "&#x1F328;",
-        "Heavy snow" : "&#x2744;",
-        "Ice pellets" : "&#x2744;",
-        "Light rain shower" : "&#x1F327;",
-        "Moderate or heavy rain shower" : "&#x1F327;",
-        "Torrential rain shower" : "&#x1F327;",
-        "Light sleet showers" : "&#x1F328;",
-        "Moderate or heavy sleet showers" : "&#x1F328;",
-        "Light snow showers" : "&#x1F328;",
-        "Moderate or heavy snow showers" : "&#x1F328;",
-        "Patchy light rain with thunder" : "&#x26C8;",
-        "Moderate or heavy rain with thunder" : "&#x26C8;",
-        "Patchy light snow with thunder" : "&#x26C8;",
-        "Moderate or heavy snow with thunder" : "&#x26C8;",
-
-
-
+    var icons = {
+        "clear" : "&#x1F324;",
+        "sunny" : "&#x2600;",
+        "partly cloudy" : "&#x26C5;",
+        "cloudy" : "&#x2601;",
+        "overcast" : "&#x2601;",
+        "mist" : "&#x1F4A7;",
+        "patchy rain possible" : "&#x1F327;",
+        "patchy snow possible" : "&#x1F328;",
+        "patchy sleet possible" : "&#x1F328;",
+        "patchy freezing drizzle possible" : "&#x1F327;",
+        "thundery outbreaks possible" : "&#x1F329;",
+        "blowing snow" : "&#x2744;",
+        "blizzard" : "&#x2744;",
+        "fog" : "&#x1F32B;",
+        "freezing fog" : "&#x1F32B;",
+        "patchy light drizzle" : "&#x1F327;",
+        "light drizzle" : "&#x1F327;",
+        "lreezing drizzle" : "&#x1F327;",
+        "heavy freezing drizzle" : "&#x1F327;",
+        "patchy light rain" : "&#x1F327;",
+        "light rain" : "&#x1F327;",
+        "rain" : "&#x1F327;",
+        "moderate rain at times" : "&#x1F327;",
+        "moderate rain" : "&#x1F327;",
+        "heavy rain at times" : "&#x1F327;",
+        "heavy rain" : "&#x1F327;",
+        "light freezing rain" : "&#x1F327;",
+        "moderate or heavy freezing rain" : "&#x1F327;",
+        "light sleet" : "&#x1F328;",
+        "moderate or heavy sleet" : "&#x1F328;",
+        "patchy light snow" : "&#x1F328;",
+        "light snow" : "&#x1F328;",
+        "patchy moderate snow" : "&#x1F328;",
+        "moderate snow" : "&#x1F328;",
+        "patchy heavy snow" : "&#x1F328;",
+        "heavy snow" : "&#x2744;",
+        "ice pellets" : "&#x2744;",
+        "light rain shower" : "&#x1F327;",
+        "moderate or heavy rain shower" : "&#x1F327;",
+        "torrential rain shower" : "&#x1F327;",
+        "light sleet showers" : "&#x1F328;",
+        "moderate or heavy sleet showers" : "&#x1F328;",
+        "light snow showers" : "&#x1F328;",
+        "moderate or heavy snow showers" : "&#x1F328;",
+        "patchy light rain with thunder" : "&#x26C8;",
+        "moderate or heavy rain with thunder" : "&#x26C8;",
+        "patchy light snow with thunder" : "&#x26C8;",
+        "moderate or heavy snow with thunder" : "&#x26C8;",
+        "snow" : "&#x1F328;",
+        "sun" : "&#x2600;",
+        "cloud" : "&#x2601;",
+        "hail" : "&#x2744;"
     
     };
-    console.log('fetching icon');
+    let iconArray = desc.split(", ");
+    desc = "snow";
 
-    return icons[desc];    
+    result = scan(icons, iconArray);
+ 
+    if ((result  == 'undefined') || (result == "")){
+        iconArray = desc.split(" ");
+        result = scan(icons, iconArray);
+    }
+
+
+    return result;    
 }
--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
-var city = "strawberry";
-var region = "land";
-var temp = 666;
-var desc = "cloudy with a chance of meatballs";
-var icon = 0;
+// var city = "strawberry";
+// var region = "land";
+// var temp = 666;
+// var desc = "cloudy with a chance of meatballs";
+// var icon = 0;
 
 function check() {
   var data = localStorage.getItem("weatherReport");
@@ -14,7 +14,7 @@ function check() {
     console.log("old is good");
   }
   data = JSON.parse(localStorage.getItem("weatherReport"));
-  var lastUpdateTime = new Date(data.time);
+  var lastUpdateTime = new Date(data.current[0].time);
   var currentTime = new Date();
 
   lastUpdateTime = new Date(lastUpdateTime.getTime() + 30 * 60000);
@@ -28,36 +28,181 @@ function updateReport() {
   console.log("updating the report");
   //fetch the weather from wttr.in
   fetch("https://wttr.in/?format=j1")
-    .then(response => response.json())
+    .then((response) => response.json())
     .then((data) => {
-      //create the JSON of the weather report
-      let desc=  data.current_condition[0].weatherDesc[0].value;
-      let icon = getIcon(desc);
-
+      console.log(data);
       let report = {
-        "time": new Date(),
-        "temp_C": data.current_condition[0].temp_C,
-        "temp_F": data.current_condition[0].temp_F,
-        "city": data.nearest_area[0].areaName[0].value,
-        "region": data.nearest_area[0].region[0].value,
-        "icon": icon,
+        current: [
+          {
+            time: new Date(),
+            temp_F: data.current_condition[0].temp_F,
+            city: data.nearest_area[0].areaName[0].value,
+            region: data.nearest_area[0].region[0].value,
+            tempC: data.current_condition[0].temp_C,
+            icon: getIcon(data.current_condition[0].weatherDesc[0].value),
+            humidity: data.current_condition[0].humidity,
+          },
+        ],
+        forecast: [
+          [
+            {
+              tempC: data.weather[0].hourly[2].tempC,
+              temp_F: data.weather[0].hourly[2].tempF,
+              desc: data.weather[0].hourly[2].weatherDesc[0].value,
+              icon: getIcon(data.weather[0].hourly[2].weatherDesc[0].value),
+              precip: data.weather[0].hourly[2].chanceofrain,
+            },
+            {
+              tempC: data.weather[0].hourly[4].tempC,
+              temp_F: data.weather[0].hourly[4].tempF,
+              desc: data.weather[0].hourly[4].weatherDesc[0].value,
+              icon: getIcon(data.weather[0].hourly[4].weatherDesc[0].value),
+              precip: data.weather[0].hourly[4].chanceofrain,
+            },
+            {
+              tempC: data.weather[0].hourly[6].tempC,
+              temp_F: data.weather[0].hourly[6].tempF,
+              desc: data.weather[0].hourly[6].weatherDesc[0].value,
+              icon: getIcon(data.weather[0].hourly[6].weatherDesc[0].value),
+              precip: data.weather[0].hourly[6].chanceofrain,
+            },
+            {
+              tempC: data.weather[0].hourly[7].tempC,
+              temp_F: data.weather[0].hourly[7].tempF,
+              desc: data.weather[0].hourly[7].weatherDesc[0].value,
+              icon: getIcon(data.weather[0].hourly[7].weatherDesc[0].value),
+              precip: data.weather[0].hourly[7].chanceofrain,
+            },
+          ],
+          [
+            {
+              tempC: data.weather[1].hourly[2].tempC,
+              temp_F: data.weather[1].hourly[2].tempF,
+              desc: data.weather[1].hourly[2].weatherDesc[0].value,
+              icon: getIcon(data.weather[1].hourly[2].weatherDesc[0].value),
+              precip: data.weather[1].hourly[2].chanceofrain,
+            },
+            {
+              tempC: data.weather[1].hourly[4].tempC,
+              temp_F: data.weather[1].hourly[4].tempF,
+              desc: data.weather[1].hourly[4].weatherDesc[0].value,
+              icon: getIcon(data.weather[1].hourly[4].weatherDesc[0].value),
+              precip: data.weather[1].hourly[4].chanceofrain,
+            },
+            {
+              tempC: data.weather[1].hourly[6].tempC,
+              temp_F: data.weather[1].hourly[6].tempF,
+              desc: data.weather[1].hourly[6].weatherDesc[0].value,
+              icon: getIcon(data.weather[1].hourly[6].weatherDesc[0].value),
+              precip: data.weather[1].hourly[6].chanceofrain,
+            },
+            {
+              tempC: data.weather[1].hourly[7].tempC,
+              temp_F: data.weather[1].hourly[7].tempF,
+              desc: data.weather[1].hourly[7].weatherDesc[0].value,
+              icon: getIcon(data.weather[1].hourly[7].weatherDesc[0].value),
+              precip: data.weather[1].hourly[7].chanceofrain,
+            },
+          ],
+          [
+            {
+              tempC: data.weather[2].hourly[2].tempC,
+              temp_F: data.weather[2].hourly[2].tempF,
+              desc: data.weather[2].hourly[2].weatherDesc[0].value,
+              icon: getIcon(data.weather[2].hourly[2].weatherDesc[0].value),
+              precip: data.weather[2].hourly[2].chanceofrain,
+            },
+            {
+              tempC: data.weather[2].hourly[4].tempC,
+              temp_F: data.weather[2].hourly[4].tempF,
+              desc: data.weather[2].hourly[4].weatherDesc[0].value,
+              icon: getIcon(data.weather[2].hourly[4].weatherDesc[0].value),
+              precip: data.weather[2].hourly[4].chanceofrain,
+            },
+            {
+              tempC: data.weather[2].hourly[6].tempC,
+              temp_F: data.weather[2].hourly[6].tempF,
+              desc: data.weather[2].hourly[6].weatherDesc[0].value,
+              icon: getIcon(data.weather[2].hourly[6].weatherDesc[0].value),
+              precip: data.weather[2].hourly[6].chanceofrain,
+            },
+            {
+              tempC: data.weather[2].hourly[7].tempC,
+              temp_F: data.weather[2].hourly[7].tempF,
+              desc: data.weather[2].hourly[7].weatherDesc[0].value,
+              icon: getIcon(data.weather[2].hourly[7].weatherDesc[0].value),
+              precip: data.weather[2].hourly[7].chanceofrain,
+            },
+          ],
+        ],
       };
 
-      //store report in an instance
+      console.log(report);
+      //store report in local storage
       localStorage.setItem("weatherReport", JSON.stringify(report));
     });
 }
 
 function updateHTML() {
+
+
+  //fetch the report
   let data = JSON.parse(localStorage.getItem("weatherReport"));
-  console.log(data);
 
-  document.getElementById("weatherIcon").innerHTML = data.icon;
-  document.getElementById("weatherTemp").innerHTML = data.temp_C + " &#x00B0;C";
-  document.getElementById("location").innerHTML = "&#x1F4CC; " + data.city + ", " + data.region;
+  //update the dates
+  let today = new Date();
+  let date_temp = new Date();
 
+  date_temp.setDate(today.getDate() + 1);
+  document.getElementById('date-1').innerHTML = date_temp.toDateString();
+
+
+  date_temp.setDate(today.getDate() + 1);
+  document.getElementById('date-2').innerHTML = date_temp.toDateString();
+
+
+
+
+  document.getElementById("weatherIcon").innerHTML = data.current[0].icon;
+  document.getElementById("weatherTemp").innerHTML =
+    data.current[0].tempC + " &#x00B0;C";
+  document.getElementById("location").innerHTML =
+    "&#x1F4CC; " + data.current[0].city + ", " + data.current[0].region;
+  document.getElementById("humidity").innerHTML = "Humidity: " +
+    data.current[0].humidity + "%";
+
+    
+
+  //update the future forecast
+
+  for (i = 1; i < 4; i++) {
+    for (j = 1; j < 5; j++) {
+      let tempName = "temp-" + i + "-" + j;
+      let iconName = "icon-" + i + "-" + j;
+      let precipName = "precip-" + i + "-" + j;
+
+      document.getElementById(tempName).innerHTML =
+        data.forecast[i - 1][j - 1].tempC;
+      document.getElementById(iconName).innerHTML =
+        data.forecast[i - 1][j - 1].icon;
+      if (data.forecast[i - 1][j - 1].precip > 5) {
+        document.getElementById(precipName).innerHTML =
+          "&#128167;" + data.forecast[i - 1][j - 1].precip + "%";
+        document.getElementById(precipName).style.display = "inline";
+      }
+    }
+  }
 }
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const chk = document.getElementById("chk");
+
+
+  chk.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+  });
+  
+
+  //remove this at launch
   check();
   updateHTML();
 });
