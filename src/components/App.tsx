0e82eff6140620011e90288f51467675cde9a184
truman126 <trumanjohnston@gmail.com>
2022-12-05 02:00:33 -0500

    working on 2.0

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/App.css'
+import Day from './Day'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Day/>
+    </div>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, rollupVersion } from 'vite'
 import react from '@vitejs/plugin-react'
+import { chromeExtension } from "vite-plugin-chrome-extension"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), chromeExtension()],
+  build: {
+    emptyOutDir: true,
+    outDir: 'dist',
+    rollupOptions: {
+      input: "src/manifest.json"
+    }
+  },
+  publicDir: 'public'
+
 })
